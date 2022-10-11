8d780390074c63a577780d448948f7a30522f95f
Kevin Wolbart <kevinwolbart@gmail.com>
2022-10-11 15:56:44 -0500

    Finished additions to RPS

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerOutputScore = 0;
+let computerOutputScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 100) + 1);
     
@@ -14,34 +17,87 @@ function playRound(e) {
     let playerSelection = e.target.className;
     let computerSelection = getComputerChoice();
 
-    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        return false;
-    }
-
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            console.log("Computer Selected Rock: It's a tie!");
+            displayScore("Computer Selected Rock: It's a tie!");
         } else if (computerSelection === "scissors") {
-            console.log("Computer Selected Scissors: You win!");
+            displayScore("Computer Selected Scissors: You win!");
         } else {
-            console.log("Computer Selected Paper: You Lose!");        }
+            displayScore("Computer Selected Paper: You Lose!");        }
     } else if (playerSelection === "Paper") {
         if (computerSelection === "rock") {
-            console.log("Computer Selected Rock: You win!");
+            displayScore("Computer Selected Rock: You win!");
         } else if (computerSelection === "scissors") {
-            console.log("Computer Selected Scissors: You lose!");
+            displayScore("Computer Selected Scissors: You lose!");
         } else {
-            console.log("Computer Selected Paper: It's a tie!");        }
+            displayScore("Computer Selected Paper: It's a tie!");        }
     } else {
         if (computerSelection === "rock") {
-            console.log("Computer Selected Rock: You lose!");
+            displayScore("Computer Selected Rock: You lose!");
         } else if (computerSelection === "scissors") {
-            console.log("Computer Selected Scissors: It's a tie!");
+            displayScore("Computer Selected Scissors: It's a tie!");
         } else {
-            console.log("Computer Selected Paper: You win!");        }
+            displayScore("Computer Selected Paper: You win!");        }
     } 
 }
 
+function displayScore(result) {
+    switch(result) {
+        case "Computer Selected Rock: It's a tie!":
+            displayMessage.textContent = "Computer Selected Rock: It's a tie!";
+            break;
+        case "Computer Selected Scissors: You win!":
+            displayMessage.textContent = "Computer Selected Scissors: You win!";
+            playerOutputScore += 1;
+            playerScore.textContent = `Player Score: ${playerOutputScore}`;
+            break;
+        case "Computer Selected Paper: You Lose!":
+            displayMessage.textContent = "Computer Selected Paper: You Lose!";
+            computerOutputScore += 1;
+            computerScore.textContent = `Computer Score: ${computerOutputScore}`;
+            break;
+        case "Computer Selected Rock: You win!":
+            displayMessage.textContent = "Computer Selected Rock: You win!";
+            playerOutputScore += 1;
+            playerScore.textContent = `Player Score: ${playerOutputScore}`;
+            break;
+        case "Computer Selected Scissors: You lose!":
+            displayMessage.textContent = "Computer Selected Scissors: You lose!";
+            computerOutputScore += 1;
+            computerScore.textContent = `Computer Score: ${computerOutputScore}`;
+            break;
+        case "Computer Selected Paper: It's a tie!":
+            displayMessage.textContent = "Computer Selected Paper: It's a tie!";
+            break;
+        case "Computer Selected Rock: You lose!":
+            displayMessage.textContent = "Computer Selected Rock: You lose!";
+            computerOutputScore += 1;
+            computerScore.textContent = `Computer Score: ${computerOutputScore}`;
+            break;
+        case "Computer Selected Scissors: It's a tie!":
+            displayMessage.textContent = "Computer Selected Scissors: It's a tie!";
+            break;
+        case "Computer Selected Paper: You win!":
+            displayMessage.textContent = "Computer Selected Paper: You win!";
+            playerOutputScore += 1;
+            playerScore.textContent = `Player Score: ${playerOutputScore}`;
+            break;
+    }
+
+    if (playerOutputScore === 5) {
+        displayMessage.textContent = "You win! Congratulations you are the Rock, Paper, Scissor's champion!";
+        playerOutputScore = 0;
+        computerOutputScore = 0;
+        playerScore.textContent = `Player Score: ${playerOutputScore}`;
+        computerScore.textContent = `Computer Score: ${computerOutputScore}`;
+    } else if(computerOutputScore === 5) {
+        displayMessage.textContent = "You lose! Congratulations you suck!";
+        playerOutputScore = 0;
+        computerOutputScore = 0;
+        playerScore.textContent = `Player Score: ${playerOutputScore}`;
+        computerScore.textContent = `Computer Score: ${computerOutputScore}`;
+    }
+}
 const rock = document.querySelector(".rock");
 rock.addEventListener("click", playRound);
 
@@ -50,6 +106,10 @@ paper.addEventListener("click", playRound);
 
 const scissors = document.querySelector(".scissors");
 scissors.addEventListener("click", playRound);
+
+const displayMessage = document.getElementById("displayMessage");
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
 
 /*function game() {
     for (let i = 0; i < 5; i++) {
